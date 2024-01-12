f1693c73a2a8dca7ceacf0243c84e7da2154b6d9
Jacob Ishak-Boushaki <124309174+Jacob-Boushaki@users.noreply.github.com>
2024-01-11 17:19:43 -0700

    Getting C++ up and running

---
+++ Hello_World_in_Cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+    cout << "You entered: " <<  num << endl;
+
+    return 0;
+
+}
