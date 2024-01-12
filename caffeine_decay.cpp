84a97a762e8068080ada83c8855a3e83483dc2cb
Jacob Ishak-Boushaki <124309174+Jacob-Boushaki@users.noreply.github.com>
2024-01-11 17:54:12 -0700

    Caffeine exercise
    This is a personal exercise in getting a c++ program up and running.

---
+++ caffeine_decay.cpp
@@ -0,0 +1,34 @@
+/*
+Caffeine Decay Exercise
+Jacob Ishak-Boushaki
+Dr. Kay
+ECE-275
+1-11-2024
+*/
+
+#include <iostream>
+#include <iomanip> // For setprecision
+#include <cmath>
+using namespace std; // This has to go here for some reason
+
+/* 
+As a reminder: A = Pe^(-rt)
+Caffeine has a half life of about 6 hours in the human body
+r = ln(2) / λ ≈≈ 0.1155
+*/
+
+int main(){
+    cout << fixed << setprecision(4);
+    double CaffeineMgInit; // Our Principle Amount
+    double CaffeineMGFinal;
+    double hours;
+
+    cout << "Enter Initial Caffeine consumption: ";
+    cin >> CaffeineMgInit;
+    cout << "Enter Hours since consumption: ";
+    cin >> hours;
+
+    CaffeineMGFinal = CaffeineMgInit * exp(-0.1155 * hours);
+
+    cout << "Final Caffeine amount in MG: " << CaffeineMGFinal;
+}
