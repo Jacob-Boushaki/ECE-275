f6fd1538acf74f7b284584c97cb74fb925f9f348
Jacob Ishak-Boushaki <124309174+Jacob-Boushaki@users.noreply.github.com>
2024-01-12 09:40:59 -0700

    improving

--- caffeine_decay.cpp
+++ caffeine_decay.cpp
@@ -10,25 +10,36 @@ ECE-275
 #include <iomanip> // For setprecision
 #include <cmath>
 using namespace std; // This has to go here for some reason
+#define HALF_LIFE 6
 
 /* 
 As a reminder: A = Pe^(-rt)
 Caffeine has a half life of about 6 hours in the human body
-r = ln(2) / λ ≈≈ 0.1155
+r = ln(2) / λ ≈≈ 0.1155 in this example
 */
+
 
 int main(){
     cout << fixed << setprecision(4);
     double CaffeineMgInit; // Our Principle Amount
     double CaffeineMGFinal;
     double hours;
+    float r = log(2) / HALF_LIFE;
+    char go = 'y';
 
-    cout << "Enter Initial Caffeine consumption: ";
-    cin >> CaffeineMgInit;
-    cout << "Enter Hours since consumption: ";
-    cin >> hours;
+    while (go == 'y'){
 
-    CaffeineMGFinal = CaffeineMgInit * exp(-0.1155 * hours);
+        cout << "Enter Initial Caffeine consumption: ";
+        cin >> CaffeineMgInit;
+        cout << "Enter Hours since consumption: ";
+        cin >> hours;
 
-    cout << "Final Caffeine amount in MG: " << CaffeineMGFinal;
+        CaffeineMGFinal = CaffeineMgInit * exp(-r * hours);
+
+        cout << "Final Caffeine amount in MG: " << CaffeineMGFinal << endl;
+
+        cout << "Go again? y or n: ";
+        cin >> go;
+        if ((go != 'y') && (go != 'n')) go = 'y';
+    }
 }
