d9fc44aad0cf329cb5b33e952c8a1970b7ec0def
Jacob Ishak-Boushaki <124309174+Jacob-Boushaki@users.noreply.github.com>
2024-01-17 23:07:31 -0700

    cleaning up repository

--- Archive/Hello_World_in_Cpp.cpp
+++ Archive/Hello_World_in_Cpp.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(){
+    cout << "Hello world" << endl;
     
     int num;
     cout << "Enter an integer: ";
---
+++ Archive/ReadMe.txt
@@ -0,0 +1,1 @@
+This is an archive for old classworks and such that I don't care to improve or revisit
